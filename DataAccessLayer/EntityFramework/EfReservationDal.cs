eb4607abfab383d9f9a222e61a682e5b910d3764
ilkaycanguder <ilkaycanguder@gmail.com>
2024-08-27 17:25:49 +0300

    Reservation-Destination Relationship

--- DataAccessLayer/EntityFramework/EfReservationDal.cs
+++ DataAccessLayer/EntityFramework/EfReservationDal.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repository;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,19 @@ namespace DataAccessLayer.EntityFramework
 {
     public class EfReservationDal : GenericRepository<Reservation>, IReservationDal
     {
+        public List<Reservation> GetListWithReservationByAccepted(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> GetListWithReservationByPrevious(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> GetListWithReservationByWaitApproval(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
