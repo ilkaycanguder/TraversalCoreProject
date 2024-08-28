3d0897ab995c4847c832a83a1ceadcbebbf865c5
ilkaycanguder <ilkaycanguder@gmail.com>
2024-08-28 15:00:42 +0300

    Create Admin Layout partial view separation

--- TraversalCoreProje/Controllers/AdminController.cs
+++ TraversalCoreProje/Controllers/AdminController.cs
@@ -13,5 +13,25 @@ namespace TraversalCoreProje.Controllers
         {
             return PartialView();
         }
+
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();   
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }
