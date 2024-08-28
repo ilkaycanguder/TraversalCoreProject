3e65359f1a245d6c8b7dcb52e584cf75b7cc451f
ilkaycanguder <ilkaycanguder@gmail.com>
2024-08-28 11:18:12 +0300

    Add platform settings view component

---
+++ TraversalCoreProje/ViewComponents/MemberDashboard/_PlatformSetting.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.MemberDashboard
+{
+    public class _PlatformSetting : ViewComponent
+    {
+        public IViewComponentResult Invoke(int id)
+        {
+            //var values = guideManager.TGetList();
+            return View();
+        }
+    }
+}
