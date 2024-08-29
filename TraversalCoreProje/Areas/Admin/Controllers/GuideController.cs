96b21fe0ed2c63443a66b09524c9f8269765f7a2
ilkaycanguder <ilkaycanguder@gmail.com>
2024-08-29 18:39:30 +0300

    Generate project Excel reports

--- TraversalCoreProje/Areas/Admin/Controllers/GuideController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/GuideController.cs
@@ -1,6 +1,9 @@
 ﻿using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace TraversalCoreProje.Areas.Admin.Controllers
 {
@@ -29,8 +32,21 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
         [HttpPost]
         public IActionResult AddGuide(Guide guide)
         {
-            _guideService.TAdd(guide);
-            return RedirectToAction("Index");
+            GuideValidator validationRules = new GuideValidator();
+            ValidationResult result = validationRules.Validate(guide);
+            if (result.IsValid)
+            {
+                _guideService.TAdd(guide);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();  
+            }
         }
 
         [HttpGet]
