2d7800b6548fa70a9af3b326c650ba3856fe0bd3
ilkaycanguder <ilkaycanguder@gmail.com>
2024-08-31 09:54:08 +0300

    Add DTO layer and AutoMapper integration

---
+++ TraversalCoreProje/Mapping/AutoMapperProfile/MapProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DTOLayer.DTOs.AnnouncementDTOs;
+using DTOLayer.DTOs.AppUserDTOs;
+using DTOLayer.DTOs.CityDTOs;
+using EntityLayer.Concrete;
+
+namespace TraversalCoreProje.Mapping.AutoMapperProfile
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<AppUserLoginDTOs, AppUser>().ReverseMap();
+            CreateMap<AppUserRegisterDTOs, AppUser>().ReverseMap();
+            CreateMap<AppUserLoginDTOs, AppUser>().ReverseMap();
+        }
+    }
+}
