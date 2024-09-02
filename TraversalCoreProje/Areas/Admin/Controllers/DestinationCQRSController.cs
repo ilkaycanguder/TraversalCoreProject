10331c481126590ac3da0ec547ca686793cb557b
ilkaycanguder <ilkaycanguder@gmail.com>
2024-09-02 18:55:13 +0300

    Implement CQRS design pattern

---
+++ TraversalCoreProje/Areas/Admin/Controllers/DestinationCQRSController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProje.CQRS.Handlers.DestinationHandlers;
+using TraversalCoreProje.CQRS.Queries.DestinationQueries;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class DestinationCQRSController : Controller
+    {
+        private readonly GetAllDestinationQueryHandler _queryHandler;
+        private readonly GetDestinationByIDQueryHandler _getDestinationByIDQueryHandler;
+
+        public DestinationCQRSController(GetAllDestinationQueryHandler queryHandler, GetDestinationByIDQueryHandler getDestinationByIDQueryHandler)
+        {
+            _queryHandler = queryHandler;
+            _getDestinationByIDQueryHandler = getDestinationByIDQueryHandler;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _queryHandler.Handle();
+            return View(values);
+        }
+
+        public IActionResult GetDestination(int id)
+        {
+            var values = _getDestinationByIDQueryHandler.Handle(new GetDestinationByIDQuery(id));
+            return View(values);    
+        }
+    }
+}
