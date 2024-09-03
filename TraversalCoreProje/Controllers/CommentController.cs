6192631f73be685efce657b36f1610de272afef7
ilkaycanguder <ilkaycanguder@gmail.com>
2024-09-03 20:08:09 +0300

    Design refinements - part 4

--- TraversalCoreProje/Controllers/CommentController.cs
+++ TraversalCoreProje/Controllers/CommentController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 
@@ -9,10 +10,19 @@ namespace TraversalCoreProje.Controllers
     public class CommentController : Controller
     {
         CommentManager commentManager = new CommentManager(new EfCommentDal());
-        [HttpGet]
-        public PartialViewResult AddComment(int id)
+        private readonly UserManager<AppUser> _userManager;
+
+        public CommentController(UserManager<AppUser> userManager)
         {
-            ViewBag.destID = id;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            //ViewBag.destID = id;
+            //var value = _userManager.FindByNameAsync(User.Identity.Name);
+            //ViewBag.userID = value.Id;
             return PartialView();
         }
 
