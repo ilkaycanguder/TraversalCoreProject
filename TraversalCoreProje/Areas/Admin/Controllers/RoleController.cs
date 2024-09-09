c8953e9c39278b142cce6247deb0ba62fc86e698
ilkaycanguder <ilkaycanguder@gmail.com>
2024-09-09 19:26:11 +0300

    List users with their roles

--- TraversalCoreProje/Areas/Admin/Controllers/RoleController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/RoleController.cs
@@ -1,6 +1,8 @@
-﻿using EntityLayer.Concrete;
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TraversalCoreProje.Areas.Admin.Models;
@@ -12,10 +14,12 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
     public class RoleController : Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public RoleController(RoleManager<AppRole> roleManager)
+        public RoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -74,6 +78,30 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
             value.Name = updateRoleViewModel.RoleName;
             await _roleManager.UpdateAsync(value);
             return RedirectToAction("Index");
+        }
+
+        public IActionResult UserList()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+            var roles = _roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel model = new RoleAssignViewModel();
+                model.RoleId = item.Id;
+                model.RoleName = item.Name;
+                model.RoleExist = userRoles.Contains(item.Name);
+                roleAssignViewModels.Add(model);    
+            }
+            return View(roleAssignViewModels);
         }
     }
 }
