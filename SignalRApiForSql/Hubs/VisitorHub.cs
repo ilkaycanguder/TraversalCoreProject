a471161fd2448ffcf3251419af2c168eddbf1f5d
ilkaycanguder <ilkaycanguder@gmail.com>
2024-09-09 15:47:23 +0300

    Display real-time data in charts using SignalR

--- SignalRApiForSql/Hubs/VisitorHub.cs
+++ SignalRApiForSql/Hubs/VisitorHub.cs
@@ -7,15 +7,13 @@ namespace SignalRApiForSql.Hubs
     public class VisitorHub : Hub
     {
         private readonly VisitorService _visitorService;
-
         public VisitorHub(VisitorService visitorService)
         {
             _visitorService = visitorService;
         }
-
         public async Task GetVisitorList()
         {
-            await Clients.All.SendAsync("CallVisitList", _visitorService.GetVisitorChartList());
+            await Clients.All.SendAsync("ReceiveVisitorList", _visitorService.GetVisitorChartList());
         }
     }
 }
