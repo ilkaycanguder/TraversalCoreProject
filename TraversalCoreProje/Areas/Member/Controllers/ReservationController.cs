e1b73fd13948f32044c07606e0058377c725e888
ilkaycanguder <ilkaycanguder@gmail.com>
2024-09-10 18:46:54 +0300

    Final revisions to member page

--- TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
@@ -66,5 +66,10 @@ namespace TraversalCoreProje.Areas.Member.Controllers
             reservationManager.TAdd(p);
             return RedirectToAction("MyCurrentReservation");
         }
+
+        public IActionResult Deneme()
+        {
+            return View();
+        }
     }
 }
